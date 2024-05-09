7c929dcb24e1268ced5c49447256d52a523387e1
知错能改天天向上 <2687279969@qq.com>
2024-05-09 16:11:58 +0800

    玩家2.0版

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,8 +5,10 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+# 从项目设定中获取重力加速度，这个值默认是980
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -14,15 +16,34 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Get the input direction and handle the movement/deceleration.-1/0/1
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	# 改变动画方向
+	if direction>0:
+		animated_sprite.flip_h=false
+	elif direction<0:
+		animated_sprite.flip_h=true
+	
+		
+		
+	# 播放动画
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else :
+			animated_sprite.play("run")
+	else :
+		animated_sprite.play("jump")
+	
+	
+	# 改变运动方向	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
